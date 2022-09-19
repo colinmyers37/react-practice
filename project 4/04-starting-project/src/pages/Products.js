5ec2bcdf0c8e46ff0e2c0c11e611195b035522bf
colinmyers37 <colinmyers37@gmail.com>
2022-09-19 16:28:58 -0500

    finished router intro

--- project 4/04-starting-project/src/pages/Products.js
+++ project 4/04-starting-project/src/pages/Products.js
@@ -1,14 +1,20 @@
 import React from "react";
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 
 function Products() {
   return (
     <section>
       <h1>Products</h1>
       <ul>
-        <li><Link to='/products/p1'>A Book</Link></li>
-        <li><Link to='/products/p2'>A Carpet</Link></li>
-        <li><Link to='/products/p3'>An Online Course</Link></li>
+        <li>
+          <Link to="/products/p1">A Book</Link>
+        </li>
+        <li>
+          <Link to="/products/p2">A Carpet</Link>
+        </li>
+        <li>
+          <Link to="/products/p3">An Online Course</Link>
+        </li>
       </ul>
     </section>
   );
--- project 4/04-starting-project/src/pages/Welcome.js
+++ project 4/04-starting-project/src/pages/Welcome.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import { Route } from 'react-router-dom'
 
 function Welcome() {
   return (
+    <section>
     <h1>The Welcome Page</h1>
+    <Route path='/welcome/new-user'>
+   <p>Welcome New User</p>   
+    </Route>
+    </section>
   )
 }
 
