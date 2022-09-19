1d737767daaa051793c2cd7de70e60890a78aa46
colinmyers37 <colinmyers37@gmail.com>
2022-09-19 14:57:40 -0500

    mid day break

--- project 4/04-starting-project/src/pages/Products.js
+++ project 4/04-starting-project/src/pages/Products.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+import { Link } from 'react-router-dom'
 
 function Products() {
   return (
-    <h1>Products</h1>
-  )
+    <section>
+      <h1>Products</h1>
+      <ul>
+        <li><Link to='/products/p1'>A Book</Link></li>
+        <li><Link to='/products/p2'>A Carpet</Link></li>
+        <li><Link to='/products/p3'>An Online Course</Link></li>
+      </ul>
+    </section>
+  );
 }
 
-export default Products
+export default Products;
