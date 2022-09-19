dc3123fd98fe712dd35f6072dc8787adfab6975d
colinmyers37 <colinmyers37@gmail.com>
2022-09-19 17:20:59 -0500

    finished day 1

---
+++ project-5/05-starting-project/src/pages/NewQuote.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function NewQuote() {
+  return (
+    <h1>New Quotes Page</h1>
+  )
+}
+
+export default NewQuote
---
+++ project-5/05-starting-project/src/pages/QuoteDetail.js
@@ -0,0 +1,19 @@
+import React, { Fragment } from "react";
+import { useParams, Route } from "react-router-dom";
+import Comments from "../components/Comments";
+
+function QuoteDetail() {
+  const params = useParams();
+
+  return (
+    <Fragment>
+      <h1>Quote Detail Page</h1>
+      <p>{params.quoteId}</p>
+      <Route path={`/quotes/${params.quoteId}/comments`}>
+        <Comments />
+      </Route>
+    </Fragment>
+  );
+}
+
+export default QuoteDetail;
